88a2faa56122ea27853361460790f4c74c7f67f8
Jorge Jair Ramirez Gaston Zuloeta <jramirzu@emeal.nttdata.com>
2022-07-06 14:47:35 -0500

    Initial Commit

---
+++ practicaGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practicaGit
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 6/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
